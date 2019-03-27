5f6f89bfb32452e533273da675fdf888b0ef3c64
bonez <bonez@mail.ru>
2019-03-27 11:26:19 +0100

    react-create-app on Heroku

---
+++ build/precache-manifest.53b76c6bf4a1d1090afd9882458b9f59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f20af2f2d5cd4d119916",
+    "url": "/static/js/main.3984af32.chunk.js"
+  },
+  {
+    "revision": "94eff2082126419d4370",
+    "url": "/static/js/2.3fcbed5b.chunk.js"
+  },
+  {
+    "revision": "f20af2f2d5cd4d119916",
+    "url": "/static/css/main.7ea3cd5e.chunk.css"
+  },
+  {
+    "revision": "8cca1ce7fa1f9f3e55ea70752008a87c",
+    "url": "/index.html"
+  }
+];
