d525af04a8573f36f3277217ec1445e2bff4507e
bonez <bonez@mail.ru>
2019-04-21 15:43:14 +0200

    Updates

---
+++ precache-manifest.827e0bd96bef5b49189395d6251c54ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "487799ae5dd1c3c965b3",
+    "url": "/redefence/static/js/runtime~main.9ff69816.js"
+  },
+  {
+    "revision": "00c11a26dc083e273d7b",
+    "url": "/redefence/static/js/main.e1543315.chunk.js"
+  },
+  {
+    "revision": "2d85c0ec27d5ae9e55b2",
+    "url": "/redefence/static/js/2.6e560eff.chunk.js"
+  },
+  {
+    "revision": "00c11a26dc083e273d7b",
+    "url": "/redefence/static/css/main.41946b94.chunk.css"
+  },
+  {
+    "revision": "abdc786d5ce73d408b2a07a0e0681cc2",
+    "url": "/redefence/index.html"
+  }
+];
